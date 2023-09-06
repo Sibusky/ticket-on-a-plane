d9cc9f0e5b56ea91a437b2cc20d1f9070ba1ce9c
Aleksei Smirnov <sibusky@yandex.ru>
2023-09-06 11:33:10 +0300

    feat: add structure for ticket

--- src/components/tickets/index.jsx
+++ src/components/tickets/index.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './styles.css'
+import React from 'react';
+import './styles.css';
 
 export function Tickets() {
   return (
-    <div className='tickets'>
-        Tickets
-    </div>
-  )
+    <ul className='tickets'>
+      <li className='tickets__item'>
+        <div className='tickets__item-buy'>
+          <img className='tickets-item-logo' alt='company logo' src='#' />
+          <button className='tickets-item-button'>Купить за 200 т.р.</button>
+        </div>
+        <div className='tickets__item-info'>
+          <div className='tickets__item-time'>
+            <p className='tickets__item-time-departure'>09:25</p>
+            <div className='tickets__item-transfer'>
+              <p className='tickets__item-transfer-counter'></p>
+              <div className='tickets__item-transfer-image'></div>
+            </div>
+            <p className='tickets__item-time-arrival'>11:25</p>
+          </div>
+          <div className='tickets__item-cities'>
+            <div className='tickets__item-departure'>
+              <p className='tickets__item-departure-airport'>VVO, Владивосток</p>
+              <p className='tickets__item-departure-date'>9 окт 2018, ПТ</p>
+            </div>
+            <div className='tickets__item-arrival'>
+              <p className='tickets__item-arrival-airport'>Тель-Авив, TLV</p>
+              <p className='tickets__item-arrival-date'>10 окт 2018, ПТ</p>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  );
 }
