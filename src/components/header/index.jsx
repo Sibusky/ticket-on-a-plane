20f3ae7df8578d5c931868a4688c8f85f20ea4fd
Aleksei Smirnov <sibusky@yandex.ru>
2023-09-07 21:42:00 +0300

    feat: add full responsive layout for filter block

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,9 @@ export function Header() {
   return (
     <header className='header section'>
       <div className='header__container section__container'>
-        <div className='header__logo'></div>
+        <div className='header__logo-wraper'>
+          <div className='header__logo'></div>
+        </div>
         <h1 className='header__title text'>AviaDetails. Все авиабилеты тут</h1>
       </div>
     </header>
