16a3f33eb1369f4eb60bb82567b40818634e237b
Aleksei Smirnov <sibusky@yandex.ru>
2023-09-07 22:34:11 +0300

    feat: add responsive layout for tickets

--- src/components/ticket/index.jsx
+++ src/components/ticket/index.jsx
@@ -69,16 +69,27 @@ export function Ticket({
         </div>
         <div className='tickets__item-cities'>
           <div className='tickets__item-origin'>
-            <p className='tickets__item-airport-name text'>
+            <p className='tickets__item-airport-name-origin tickets__item-airport-name text'>
               {origin}, {origin_name}
             </p>
             <p className='tickets__item-date text'>{departure_date}</p>
           </div>
           <div className='tickets__item-destination'>
-            <p className='tickets__item-airport-name text'>
+            <p className='tickets__item-airport-name-destination tickets__item-airport-name text'>
               {destination_name}, {destination}
             </p>
             <p className='tickets__item-date text'>{arrival_date}</p>
+          </div>
+        </div>
+        <div className='tickets__item-stops-wraper'>
+          <p className='tickets__item-stops text'>{formatTransfers(stops)}</p>
+          <div className='tickets__item-plane'>
+            <svg xmlns='http://www.w3.org/2000/svg' width='313' height='305' viewBox='0 0 313 305'>
+              <path
+                fill='gray'
+                d='m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47'
+              />
+            </svg>
           </div>
         </div>
       </div>
