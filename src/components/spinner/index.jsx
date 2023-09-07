dc837060abc6c4c74e0584071e36e268f9220942
Aleksei Smirnov <sibusky@yandex.ru>
2023-09-07 15:19:00 +0300

    feat: add spinner styles

--- src/components/spinner/index.jsx
+++ src/components/spinner/index.jsx
@@ -3,21 +3,19 @@ import './styles.css';
 
 export function Spinner() {
   return (
-    <div className='spinner-wraper'>
-      <div className='spinner'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+    <div className='spinner'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
